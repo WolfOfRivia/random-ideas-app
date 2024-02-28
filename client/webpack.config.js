3849d3a29d0263bd2b7b5e828ec03c07effab899
AJ Harris <shogunaut90@gmail.com>
2024-02-27 20:30:30 -0600

    changed webpack env back to production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 ("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
+  // mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../public'),
